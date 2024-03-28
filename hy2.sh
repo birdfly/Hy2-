5c56a942b25cd836f30cb5a25309c3a65fbee254
Birdfly <junshuaiw@gmail.com>
2024-03-28 21:01:44 +0800

    Update hy2.sh
    Use Runit

--- hy2.sh
+++ hy2.sh
@@ -1,26 +1,43 @@
 #!/bin/sh
 
-# 安装必要的软件包
-if [ -f "/usr/bin/apt-get" ]; then
-  apt-get update -y && apt-get upgrade -y
-  apt-get install -y gawk curl runit
-else
-  yum update -y && yum upgrade -y
-  yum install -y epel-release
-  yum install -y gawk curl runit
+# forum: https://1024.day
+
+if [[ $EUID -ne 0 ]]; then
+  clear
+  echo "Error: This script must be run as root!" 1>&2
+  exit 1
 fi
 
-# 下载并安装 Hysteria
-wget https://github.com/apernet/hysteria/releases/download/app%2Fv2.4.0/hysteria-linux-amd64 -O /usr/local/bin/hysteria-server
-chmod +x /usr/local/bin/hysteria-server
+timedatectl set-timezone Asia/Shanghai
 
-# 创建 Hysteria 配置目录
-mkdir -p /etc/hysteria/
-
-# 生成 Hysteria 配置文件
 hyPasswd=$(cat /proc/sys/kernel/random/uuid)
 getPort=$(shuf -i 2000-65000 -n 1)
-cat >/etc/hysteria/config.yaml <<EOF
+
+getIP() {
+  local serverIP=
+  serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+  if [[ -z "${serverIP}" ]]; then
+    serverIP=$(curl -s -6 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+  fi
+  echo "${serverIP}"
+}
+
+install_hy2() {
+  if [ -f "/usr/bin/apt-get" ]; then
+    apt-get update -y && apt-get upgrade -y
+    apt-get install -y gawk curl runit
+  else
+    yum update -y && yum upgrade -y
+    yum install -y epel-release
+    yum install -y gawk curl runit
+  fi
+
+  bash <(curl -fsSL https://get.hy2.sh/)
+
+  mkdir -p /etc/hysteria/
+  openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && chown hysteria /etc/hysteria/server.key && chown hysteria /etc/hysteria/server.crt
+
+  cat >/etc/hysteria/config.yaml <<EOF
 listen: :$getPort
 tls:
   cert: /etc/hysteria/server.crt
@@ -40,34 +57,33 @@ quic:
   maxConnReceiveWindow: 67108864
 EOF
 
-# 生成 TLS 证书
-openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && chown hysteria /etc/hysteria/server.key && chown hysteria /etc/hysteria/server.crt
-
-# 配置 runit
-cat >/etc/service/hysteria/run <<EOF
+  mkdir -p /etc/service/hysteria
+  cat >/etc/service/hysteria/run <<EOF
 #!/bin/sh
 exec /usr/local/bin/hysteria-server -config /etc/hysteria/config.yaml
 EOF
 chmod +x /etc/service/hysteria/run
-
-# 启动 Hysteria 服务
 sv start hysteria
+}
 
-# 显示配置信息
-hylink=$(echo -n "${hyPasswd}@$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "=" '{print $2}'):${getPort}/?insecure=1&sni=bing.com#1024-Hysteria2")
+client_hy2() {
+  hylink=$(echo -n "${hyPasswd}@$(getIP):${getPort}/?insecure=1&sni=bing.com#1024-Hysteria2")
+  echo
+  echo "安装已经完成"
+  echo
+  echo "===========Hysteria2配置参数============"
+  echo
+  echo "地址：$(getIP)"
+  echo "端口：${getPort}"
+  echo "密码：${hyPasswd}"
+  echo "SNI：bing.com"
+  echo "传输协议：tls"
+  echo "打开跳过证书验证，true"
+  echo
+  echo "========================================="
+  echo "hysteria2://${hylink}"
+  echo
+}
 
-echo
-echo "安装已经完成"
-echo
-echo "===========Hysteria2配置参数============"
-echo
-echo "地址：$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "=" '{print $2}')"
-echo "端口：${getPort}"
-echo "密码：${hyPasswd}"
-echo "SNI：bing.com"
-echo "传输协议：tls"
-echo "打开跳过证书验证，true"
-echo
-echo "========================================="
-echo "hysteria2://${hylink}"
-echo
+install_hy2
+client_hy2
