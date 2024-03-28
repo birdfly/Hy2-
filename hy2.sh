3aca935c56123dcf481cf8e43550991755ef2ae5
Birdfly <junshuaiw@gmail.com>
2024-03-28 21:13:33 +0800

    Update hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -1,53 +1,48 @@
 #!/bin/sh
-
 # forum: https://1024.day
 
-if [[ $EUID -ne 0 ]]; then
-  clear
-  echo "Error: This script must be run as root!" 1>&2
-  exit 1
+if [ "$(id -u)" -ne 0 ]; then
+    clear
+    echo "Error: This script must be run as root!" 1>&2
+    exit 1
 fi
 
 timedatectl set-timezone Asia/Shanghai
-
 hyPasswd=$(cat /proc/sys/kernel/random/uuid)
 getPort=$(shuf -i 2000-65000 -n 1)
 
-getIP() {
-  local serverIP=
-  serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
-  if [[ -z "${serverIP}" ]]; then
-    serverIP=$(curl -s -6 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
-  fi
-  echo "${serverIP}"
+getIP(){
+    local serverIP=
+    serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+    if [ -z "${serverIP}" ]; then
+        serverIP=$(curl -s -6 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "[=]" '{print $2}')
+    fi
+    echo "${serverIP}"
 }
 
-install_dependencies() {
-  if [ -f "/usr/bin/apt-get" ]; then
-    apt-get update -y && apt-get upgrade -y
-    apt-get install -y gawk curl runit
-  else
-    yum update -y && yum upgrade -y
-    yum install -y epel-release
-    yum install -y gawk curl runit
-  fi
-}
+install_hy2(){
+    if command -v apt-get &>/dev/null; then
+        apt-get update -y && apt-get upgrade -y
+        apt-get install -y gawk curl runit
+    elif command -v yum &>/dev/null; then
+        yum update -y && yum upgrade -y
+        yum install -y epel-release
+        yum install -y gawk curl runit
+    fi
+    bash <(curl -fsSL https://get.hy2.sh/)
+    mkdir -p /etc/hysteria/
+    openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500 && chown hysteria /etc/hysteria/server.key && chown hysteria /etc/hysteria/server.crt
 
-install_hy2() {
-  bash <(curl -fsSL https://get.hy2.sh/)
-
-  mkdir -p /etc/hysteria/
-  openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500
-  chmod 600 /etc/hysteria/server.key /etc/hysteria/server.crt
-
-  cat >/etc/hysteria/config.yaml <<EOF
+cat >/etc/hysteria/config.yaml <<EOF
 listen: :$getPort
 tls:
   cert: /etc/hysteria/server.crt
   key: /etc/hysteria/server.key
+
 auth:
   type: password
   password: $hyPasswd
+
 masquerade:
   type: proxy
   proxy:
@@ -60,34 +55,29 @@ quic:
   maxConnReceiveWindow: 67108864
 EOF
 
-  mkdir -p /etc/service/hysteria
-  cat >/etc/service/hysteria/run <<EOF
+    # 设置runit服务
+    HYSTERIA_SERVICE_DIR="/etc/sv/hysteria-server"
+    mkdir -p "${HYSTERIA_SERVICE_DIR}"
+    cat <<EOF >"${HYSTERIA_SERVICE_DIR}/run"
 #!/bin/sh
-exec /usr/local/bin/hysteria-server -config /etc/hysteria/config.yaml
+exec 2>&1
+exec /usr/local/bin/hysteria server -config /etc/hysteria/config.yaml
 EOF
-chmod +x /etc/service/hysteria/run
-sv start hysteria
+    chmod +x "${HYSTERIA_SERVICE_DIR}/run"
+
+    # 启动runit服务
+    ln -s "${HYSTERIA_SERVICE_DIR}" /etc/service/
 }
 
-client_hy2() {
-  hylink=$(echo -n "${hyPasswd}@$(getIP):${getPort}/?insecure=1&sni=bing.com#1024-Hysteria2")
-  echo
-  echo "安装已经完成"
-  echo
-  echo "===========Hysteria2配置参数============"
-  echo
-  echo "地址：$(getIP)"
-  echo "端口：${getPort}"
-  echo "密码：${hyPasswd}"
-  echo "SNI：bing.com"
-  echo "传输协议：tls"
-  echo "打开跳过证书验证，true"
-  echo
-  echo "========================================="
-  echo "hysteria2://${hylink}"
-  echo
-}
+client_hy2(){
+    hylink=$(echo -n "${hyPasswd}@$(getIP):${getPort}/?insecure=1&sni=bing.com#1024-Hysteria2")
 
-install_dependencies
-install_hy2
-client_hy2
+    echo
+    echo "安装已经完成"
+    echo
+    echo "===========Hysteria2配置参数============"
+    echo
+    echo "地址：$(getIP)"
+    echo "端口：${getPort}"
+    echo "密码：${hyPasswd}"
+    echo "S
