640e9d86cebeecc3da37bf47b16bda34ef8a9613
Birdfly <junshuaiw@gmail.com>
2024-03-28 21:47:44 +0800

    Update hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -1,80 +1,73 @@
 #!/bin/sh
+# forum: https://1024.day
 
-# 确保脚本以root权限执行
 if [ "$(id -u)" -ne 0 ]; then
+    clear
     echo "Error: This script must be run as root!" 1>&2
     exit 1
 fi
 
-# 设置时区为Asia/Shanghai
 timedatectl set-timezone Asia/Shanghai
-
-# 生成随机密码和端口号
 hyPasswd=$(cat /proc/sys/kernel/random/uuid)
 getPort=$(shuf -i 2000-65000 -n 1)
 
-# 函数：获取服务器IP
 getIP(){
     local serverIP=
-    serverIP=$(curl -s -4 https://ipinfo.io/ip)
+    serverIP=$(curl -s -4 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "=" '{print $2}')
     if [ -z "${serverIP}" ]; then
-        serverIP=$(curl -s -6 https://ipinfo.io/ip)
+        serverIP=$(curl -s -6 http://www.cloudflare.com/cdn-cgi/trace | grep "ip" | awk -F "=" '{print $2}')
     fi
     echo "${serverIP}"
 }
 
-# 安装Hysteria和runit
-install_hy(){
-    # 安装基本工具
-    apt-get update -y && apt-get install -y curl runit openssl
-
-    # 下载和安装Hysteria
+install_hy2(){
+    if [ -f "/usr/bin/apt-get" ]; then
+        apt-get update -y && apt-get upgrade -y
+        apt-get install -y gawk curl
+    else
+        yum update -y && yum upgrade -y
+        yum install -y epel-release
+        yum install -y gawk curl
+    fi
     bash <(curl -fsSL https://get.hy2.sh/)
-
-    # 创建证书
     mkdir -p /etc/hysteria/
-    openssl req -x509 -nodes -newkey rsa:4096 -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500
-
-    # 创建Hysteria配置文件
-    cat > /etc/hysteria/config.json <<EOF
-{
-    "listen": ":$getPort",
-    "protocol": "quic",
-    "obfs": "bing.com",
-    "cert": "/etc/hysteria/server.crt",
-    "key": "/etc/hysteria/server.key",
-    "auth": {
-        "mode": "password",
-        "config": {
-            "passwords": [
-                "$hyPasswd"
-            ]
-        }
-    }
-}
-EOF
-
-    # 设置runit服务
-    mkdir -p /etc/sv/hysteria
-    cat > /etc/sv/hysteria/run <<EOF
+    openssl req -x509 -nodes -newkey ec:<(openssl ecparam -name prime256v1) -keyout /etc/hysteria/server.key -out /etc/hysteria/server.crt -subj "/CN=bing.com" -days 36500
+    
+    # 创建runit服务脚本
+    HYSTERIA_SERVICE_DIR="/etc/sv/hysteria"
+    mkdir -p "${HYSTERIA_SERVICE_DIR}"
+    cat <<EOF >"${HYSTERIA_SERVICE_DIR}/run"
 #!/bin/sh
-exec 2>&1
-exec /usr/local/bin/hysteria -config /etc/hysteria/config.json
+exec /usr/local/bin/hysteria server -config /etc/hysteria/config.yaml
 EOF
-    chmod +x /etc/sv/hysteria/run
+    chmod +x "${HYSTERIA_SERVICE_DIR}/run"
+    
+    # 激活runit服务
+    ln -s "${HYSTERIA_SERVICE_DIR}" /etc/service/
 
-    # 激活和启动服务
-    ln -s /etc/sv/hysteria /etc/service/
+    # 清理不再需要的脚本
+    rm -f tcp-wss.sh hy2.sh
 }
 
-# 安装并启动Hysteria
-install_hy
+client_hy2(){
+    hylink=$(echo -n "${hyPasswd}@$(getIP):${getPort}/?insecure=1&sni=bing.com#1024-Hysteria2")
 
-# 显示客户端配置信息
-echo
-echo "Hysteria安装和配置完成。"
-echo "配置信息："
-echo "服务器地址: $(getIP)"
-echo "端口: $getPort"
-echo "密码: $hyPasswd"
-echo "伪装域名: bing.com"
+    echo
+    echo "安装已经完成"
+    echo
+    echo "===========Hysteria2配置参数============"
+    echo
+    echo "地址：$(getIP)"
+    echo "端口：${getPort}"
+    echo "密码：${hyPasswd}"
+    echo "SNI：bing.com"
+    echo "传输协议：tls"
+    echo "打开跳过证书验证，true"
+    echo
+    echo "========================================="
+    echo "hysteria2://${hylink}"
+    echo
+}
+
+install_hy2
+client_hy2
